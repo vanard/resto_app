3420bd0780d809e94cb0901405dabcc273c5c97f
vanard <vianixa34@gmail.com>
2025-08-05 08:48:02 +0700

    add search feature

--- lib/screens/restaurant_food_view.dart
+++ lib/screens/restaurant_food_view.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/models/restaurant.dart';
 import 'package:restaurant_app/providers/restaurants_provider.dart';
-import 'package:restaurant_app/widgets/error_component.dart';
+import 'package:restaurant_app/utils/debouncer.dart';
 import 'package:restaurant_app/widgets/restaurant_item.dart';
 
 class RestaurantFoodScreens extends StatefulWidget {
@@ -15,11 +15,30 @@ class RestaurantFoodScreens extends StatefulWidget {
 }
 
 class _RestaurantFoodScreensState extends State<RestaurantFoodScreens> {
+  var _isSearching = false;
+  final _searchTextController = TextEditingController();
+  final _debouncer = Debouncer(milliseconds: 500);
+
+  void _onSearchChanged(String query) {
+    _debouncer.run(() {
+      final provider = Provider.of<RestaurantsProvider>(context, listen: false);
+      provider.searchRestaurants(query);
+      debugPrint("Performing search for: $query");
+    });
+  }
+
   @override
   void initState() {
     final provider = Provider.of<RestaurantsProvider>(context, listen: false);
-    provider.getRestaurantList();
+    provider.fetchRestaurantList();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchTextController.dispose();
+    _debouncer.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,7 +47,31 @@ class _RestaurantFoodScreensState extends State<RestaurantFoodScreens> {
     final provider = Provider.of<RestaurantsProvider>(context);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Restaurant')),
+      appBar: AppBar(
+        title: _isSearching
+            ? TextField(
+                onSubmitted: _onSearchChanged,
+                controller: _searchTextController,
+                decoration: InputDecoration(
+                  hintText: 'Search...',
+                  border: InputBorder.none,
+                ),
+                autofocus: true,
+                onChanged: _onSearchChanged,
+              )
+            : Text('Restaurant'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _searchTextController.text = '';
+                _isSearching = !_isSearching;
+              });
+            },
+            icon: _isSearching ? Icon(Icons.close) : Icon(Icons.search),
+          ),
+        ],
+      ),
       body: provider.isLoading
           ? getLoadingUI()
           : provider.error.isNotEmpty
@@ -39,6 +82,10 @@ class _RestaurantFoodScreensState extends State<RestaurantFoodScreens> {
 
   Widget getLoadingUI() {
     return const Center(child: CircularProgressIndicator());
+  }
+
+  Widget getErrorUI(String error) {
+    return Center(child: Text(error));
   }
 
   getBodyUI(List<Restaurant> restaurants) {
