b123234da3676dd48aa8467012d9d2b1eab32f70
vanard <vianixa34@gmail.com>
2025-08-05 08:45:18 +0700

    update provider add function to add review and fetch search result

--- lib/providers/restaurants_provider.dart
+++ lib/providers/restaurants_provider.dart
@@ -1,22 +1,29 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:restaurant_app/data/api.dart';
+import 'package:restaurant_app/data/api_service.dart';
 import 'package:restaurant_app/models/base_response.dart';
+import 'package:restaurant_app/models/customer_review.dart';
 import 'package:restaurant_app/models/restaurant.dart';
 
 class RestaurantsProvider extends ChangeNotifier {
-  bool isLoading = true;
-  String error = '';
+  late final ApiService _apiService;
+  bool _isLoading = true;
+  String _error = '';
+  Map<String, dynamic>? _remoteData;
 
-  BaseResponseData baseResponseData = BaseResponseData(
+  RestaurantsProvider({required ApiService apiService})
+    : _apiService = apiService;
+
+  bool get isLoading => _isLoading;
+  String get error => _error;
+  Map<String, dynamic>? get data => _remoteData;
+
+  ResponseRestaurants baseResponseData = ResponseRestaurants(
     error: false,
     message: '',
     restaurants: [],
   );
 
-  BaseResponse baseResponse = BaseResponse(
+  ResponseRestaurant baseResponse = ResponseRestaurant(
     error: false,
     message: '',
     restaurant: Restaurant(
@@ -29,44 +36,89 @@ class RestaurantsProvider extends ChangeNotifier {
     ),
   );
 
-  void getRestaurantList() async {
+  ResponseReviews responseReviews = ResponseReviews(
+    error: false,
+    message: '',
+    reviews: [],
+  );
+
+  void fetchRestaurantList() async {
+    _isLoading = true;
+
     try {
-      final response = await http.get(Uri.parse('${Api.base_url}/list'));
-      debugPrint(response.body);
-      debugPrint(response.statusCode.toString());
-      if (response.statusCode == 200) {
-        final resData = jsonDecode(response.body);
-        baseResponseData = BaseResponseData.fromJson(resData);
-      } else {
-        throw Exception(
-          'status code: ${response.statusCode}\nSomething went wrong. Please try again later.',
-        );
-      }
+      _remoteData = await _apiService.getRestaurantList();
+      baseResponseData = ResponseRestaurants.fromJson(_remoteData!);
+      _error = '';
     } catch (e) {
-      // error = e.toString();
-      error = 'Error while fetching data. Please try again later.';
+      debugPrint('Error: $e');
+      _error = ApiService.messageToUser;
+      _remoteData = null;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
     }
-    isLoading = false;
-    notifyListeners();
   }
 
-  void getRestaurantById(String id) async {
+  void fetchRestaurantById(String id) async {
+    _isLoading = true;
+
     try {
-      final response = await http.get(Uri.parse('${Api.base_url}/detail/$id'));
-      debugPrint(response.body);
-      debugPrint(response.statusCode.toString());
-      if (response.statusCode == 200) {
-        final resData = jsonDecode(response.body);
-        baseResponse = BaseResponse.fromJson(resData);
-      } else {
-        throw Exception(
-          'status code: ${response.statusCode}\nSomething went wrong. Please try again later.',
+      _remoteData = await _apiService.getRestaurantById(id);
+      baseResponse = ResponseRestaurant.fromJson(_remoteData!);
+      _error = '';
+    } catch (e) {
+      debugPrint('Error: $e');
+      _error = ApiService.messageToUser;
+      _remoteData = null;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> addCustomerReview(String restoId, CustomerReview review) async {
+    _isLoading = true;
+
+    try {
+      _remoteData = await _apiService.addReview(restoId, review);
+      responseReviews = ResponseReviews.fromJson(_remoteData!);
+      baseResponse.restaurant.customerReviews = responseReviews.reviews;
+      _error = '';
+    } catch (e) {
+      debugPrint('Error: $e');
+      _error = ApiService.messageToUser;
+      _remoteData = null;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void searchRestaurants(String query) async {
+    _isLoading = true;
+
+    try {
+      _remoteData = await _apiService.searchRestaurant(query);
+      final founded = _remoteData!['founded'];
+      debugPrint('Found: ${_remoteData!['founded']}');
+      if (founded == 0) {
+        baseResponseData = ResponseRestaurants(
+          error: false,
+          message: '',
+          restaurants: [],
         );
+        _error = ApiService.emptyRestaurantList;
+      } else {
+        baseResponseData = ResponseRestaurants.fromJson(_remoteData!);
+        _error = '';
       }
     } catch (e) {
-      error = 'Error while fetching data. Please try again later.';
+      debugPrint('Error: $e');
+      _error = ApiService.messageToUser;
+      _remoteData = null;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
     }
-    isLoading = false;
-    notifyListeners();
   }
 }
